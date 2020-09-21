66e7b35f949b6b6d0bdbe547105ec8066c0f64c2
Theo Fanet <tfanet@tankyou.com>
2020-09-21 15:33:33 +0200

    added Camera2D

--- src/cameras.ts
+++ src/cameras.ts
@@ -1,5 +1,5 @@
-import { ICamera, ICamera3D } from "./interfaces/cameras";
-import { vec3, mat4, glMatrix, vec4 } from "gl-matrix";
+import { ICamera, ICamera3D, ICamera2D } from "./interfaces/cameras";
+import { vec3, mat4, glMatrix } from "gl-matrix";
 import { GLContext } from "./core";
 
 
@@ -15,32 +15,83 @@ class Camera implements ICamera {
         this.RecalculateProjectionViewMatrix();
     }
 
-    RecalculateProjectionViewMatrix = (): void => {
-        console.log("JJ");
+    RecalculateViewMatrix = (): void => { throw new Error("Not implemented;"); };
+    RecalculateProjectionMatrix = (): void => { throw new Error("Not implemented;"); };
+
+    RecalculateProjectionViewMatrix = (): void => { 
+        this.ProjectionViewMatrix = mat4.create();
+        mat4.multiply(this.ProjectionViewMatrix, this.ProjectionMatrix, this.ViewMatrix);
     };
 }
 
 class Camera3D extends Camera implements ICamera3D {
     Up: vec3 = [0, 1, 0];
     Front: vec3 = [0, 0, 0];
+    FOV: number = 90.0;
 
     constructor(){
         super();
-        const { canvas } = GLContext;
-        mat4.perspective(this.ProjectionMatrix, glMatrix.toRadian(90.0), canvas.width/canvas.height, 0.1, 100.0);
+        this.RecalculateProjectionMatrix();
+        this.RecalculateViewMatrix();
     }
 
-    RecalculateProjectionViewMatrix = (): void => {
+    SetFOV = (FOV: number): void => {
+        this.FOV = FOV;
+        this.RecalculateProjectionMatrix();
+    }
+
+    RecalculateViewMatrix = (): void => {
         this.ViewMatrix = mat4.create();
-        this.ProjectionViewMatrix = mat4.create();
         mat4.lookAt(this.ViewMatrix, this.Position, this.Front, this.Up);
-        mat4.multiply(this.ProjectionViewMatrix, this.ProjectionMatrix, this.ViewMatrix);
-        console.log(this.Position);
+        this.RecalculateProjectionViewMatrix();
+    }
+
+    RecalculateProjectionMatrix = (): void => {
+        const { canvas } = GLContext;
+        mat4.perspective(this.ProjectionMatrix, glMatrix.toRadian(this.FOV), canvas.width/canvas.height, 0.1, 100.0);
+        this.RecalculateProjectionViewMatrix();
+    }
+}
+
+class Camera2D extends Camera implements ICamera2D {
+    Rotation: vec3 = [0, 0, 0];
+    ZoomLevel: number = 1.0;
+
+    SetZoomLevel = (zoom: number): void => {
+        this.ZoomLevel = zoom;
+        this.RecalculateProjectionMatrix();
     };
+
+    SetRotation = (rotation: vec3): void => {
+        this.Rotation = rotation;
+        this.RecalculateViewMatrix();
+    }
+
+    RecalculateViewMatrix = (): void => {
+        this.ViewMatrix = mat4.create();
+        mat4.identity(this.ViewMatrix);
+        mat4.translate(this.ViewMatrix, this.ViewMatrix, this.Position);
+        mat4.rotate(this.ViewMatrix, this.ViewMatrix, this.Rotation[0], [1, 0, 0]);
+        mat4.rotate(this.ViewMatrix, this.ViewMatrix, this.Rotation[1], [0, 1, 0]);
+        mat4.rotate(this.ViewMatrix, this.ViewMatrix, this.Rotation[2], [0, 0, 1]);
+        mat4.invert(this.ViewMatrix, this.ViewMatrix);
+        this.RecalculateProjectionViewMatrix();
+    }
+
+    RecalculateProjectionMatrix = (): void => {
+        const { canvas } = GLContext;
+        let aspectRatio = canvas.width / canvas.height;
+        mat4.ortho(this.ProjectionMatrix, 
+            -aspectRatio * this.ZoomLevel, aspectRatio * this.ZoomLevel, 
+            -this.ZoomLevel, this.ZoomLevel, 
+            -1.0, 1.0
+        );
+        this.RecalculateProjectionViewMatrix();
+    }
 }
 
 
 export { 
     Camera,
-    Camera3D
+    Camera2D, Camera3D
 }   
--- src/interfaces/cameras.ts
+++ src/interfaces/cameras.ts
@@ -10,14 +10,28 @@ interface ICamera {
     ViewMatrix: mat4,
     ProjectionViewMatrix: mat4,
 
+    RecalculateViewMatrix(): void,
+    RecalculateProjectionMatrix(): void,
     RecalculateProjectionViewMatrix(): void
 };
 
 interface ICamera3D extends ICamera {
+    FOV: Number,
     Up: vec3, 
-    Front: vec3
+    Front: vec3,
+
+    SetFOV(FOV: number): void
+}
+
+interface ICamera2D extends ICamera {
+    Rotation: vec3,
+    ZoomLevel: number,
+
+    SetZoomLevel(zoom: number): void,
+    SetRotation(rotation: vec3): void
 }
 
 export {
-    ICamera, ICamera3D
+    ICamera, 
+    ICamera2D, ICamera3D
 }
